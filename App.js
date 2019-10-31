105f23e10a4546c60b70eeef04cc8c938c35f248
mathith <mathith@stud.ntnu.no>
2019-10-31 20:37:44 +0100

    Set up redux, partly resolve issue #14

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { ThemeProvider, Button } from "react-native-elements";
+import { View } from "react-native";
+import { ThemeProvider } from "react-native-elements";
 import SortContainer from "./components/SortContainer";
 
 const theme = {

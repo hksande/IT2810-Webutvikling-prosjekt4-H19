d57dcb4c62e58b2e08af0fb88896be9f62db5d67
johanlie <johanlie@stud.ntnu.no>
2019-11-06 10:06:44 +0100

    Started frontpage, partly resolve issue #18

---
+++ components/Frontpage.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+
+
+export default class Frontpage extends Component {
+  render() {
+    <View>
+      <Box category = "All" uri = 'https://images.unsplash.com/photo-1557149559-d74af2d38a1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60'/>
+      <Box category = "Redwine" uri = 'https://images.unsplash.com/photo-1551980362-5f5816fdf020?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60'/>
+      <Box category = "Whitewine" uri = 'https://images.unsplash.com/photo-1458945037814-389ec6994cbd?ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'/>
+      <Box category = "Sparkling" uri = 'https://images.unsplash.com/photo-1548496094-354c8b7ec28c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60'/>
+      <Box category = "Beer" uri = 'https://images.unsplash.com/photo-1518099074172-2e47ee6cfdc0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60' />
+      <Box category = "Vodka" uri = 'https://images.unsplash.com/photo-1523905491727-d82018a34d75?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'/>
+    </View>
+  }
+}
+
+    /*renderer(){
+    return(
+    <Image
+    style={{
+      width: 150,
+      height: 150,
+      resizeMode: "contain",
+      overflow: "hidden",
+      name = "redwine"
+    }}
+    source= {{ uri:'https://images.unsplash.com/photo-1551980362-5f5816fdf020?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60'}}
+  />
+
+  <Image
+  style={{
+    width: 150,
+    height: 150,
+    resizeMode: "contain",
+    overflow: "hidden",
+    name = "whitewine"
+  }}
+  source= {{ uri:'https://images.unsplash.com/photo-1458945037814-389ec6994cbd?ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'}}
+/>   <Image
+    style={{
+      width: 150,
+      height: 150,
+      resizeMode: "contain",
+      overflow: "hidden",
+      name = "sparkelig"
+    }}
+    source= {{ uri:'https://images.unsplash.com/photo-1548496094-354c8b7ec28c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60'}}
+  />   <Image
+  style={{
+    width: 150,
+    height: 150,
+    resizeMode: "contain",
+    overflow: "hidden",
+    name = "beer"
+  }}
+  source= {{ uri:'https://images.unsplash.com/photo-1518099074172-2e47ee6cfdc0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60'}}
+/>   <Image
+    style={{
+      width: 150,
+      height: 150,
+      resizeMode: "contain",
+      overflow: "hidden",
+      name = "vodka"
+    }}
+    source= {{ uri:'https://images.unsplash.com/photo-1523905491727-d82018a34d75?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'}}
+    />
+    )}}*/
+}
